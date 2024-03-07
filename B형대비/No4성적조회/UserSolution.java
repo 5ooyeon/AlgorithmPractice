a1118e0e91f580ee476f8a1c9eae92dd933d5f34
5ooyeon <symelcho@inu.ac.kr>
2024-03-08 00:06:26 +0900

    24.03.08

--- B형대비/No4성적조회/UserSolution.java
+++ B형대비/No4성적조회/UserSolution.java
@@ -1,11 +1,10 @@
 package B형대비.No4성적조회;
 
 import java.util.ArrayList;
+import java.util.TreeMap;
 
 class UserSolution {
-
-    Student[] mIdSort = new Student[1000000000];
-    ArrayList<Student>[] gradeSort = new ArrayList[3];
+    static TreeMap<Integer, Student>[] map = new TreeMap[6];
 
     static class Student{
         int mId;
@@ -36,24 +35,44 @@ class UserSolution {
     }
 
 	public void init() {
-        for(int i=0;i<3;i++) {
-            gradeSort[i] = new ArrayList<>();
-        }
 		return;
 	}
 
 	public int add(int mId, int mGrade, char mGender[], int mScore) {
-        mIdSort[mId-1] = new Student(mId, mGrade, mGender, mScore);
+        int idx = (mGrade-1)*2;
+        if(mGender[0] == 'f') {
+            idx++;
+        }
+        map[idx].put(mId, new Student(mId, mGrade, mGender, mScore));
         //mGrade학년의 mGender인 학생 중 성적 가장 높은 학생의 ID 출력
-		return 0;
+		return map[idx].lastKey();
 	}
 
 	public int remove(int mId) {
-        
+        for(int i=0;i<6;i++) {
+            Student temp = map[i].get(mId);
+            if(temp!=null) {
+                if(temp.mScore == -1) {
+                    return 0;
+                } else {
+                    temp.mScore = -1;
+                    return map[i].firstKey();
+                }
+            }
+        }
 		return 0;
 	}
 
 	public int query(int mGradeCnt, int mGrade[], int mGenderCnt, char mGender[][], int mScore) {
-		return 0;
+		for(int a : mGrade) {
+            int idx = (a-1)*2;
+            for(int b=0;b < mGenderCnt;b++) {
+                if(mGender[b][0] == 'f') {
+                    idx++;
+                }
+                
+            }
+        }
+        return 0;
 	}
 }
