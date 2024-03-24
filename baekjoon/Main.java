1a1695d45e9b32147ac6d3fe81127a4e26c2029a
5ooyeon <symelcho@inu.ac.kr>
2024-03-25 00:16:34 +0900

    24.03.24

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -1,67 +1,47 @@
 package baekjoon;
-
-import java.io.*;
-import java.util.*;
+    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int [][]arr;
+    static int []dx = {-1,1,0,0};
+    static int []dy = {0,0,-1,1};
+    static int n;
+    static boolean [][] check;
+    static int sum ;
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(bf.readLine());
-
-        for (int tc = 1; tc <= T; tc++) {
-            String func = bf.readLine();
-            int size = Integer.parseInt(bf.readLine());
-            boolean isReversed = false, isError = false;
-            Deque<String> deque = new LinkedList<>();
-
-            String input = bf.readLine();
-            input = input.substring(1, input.length() - 1);
-            StringTokenizer st = new StringTokenizer(input, ",");
-
-            for (int i = 0; i < size; i++) {
-                deque.offer(st.nextToken());
-            }
-
-            int cnt = (int) func.chars().filter(ch -> ch == 'D').count();
-
-            if (cnt > size) {
-                sb.append("error\n");
-                continue;
-            }
-
-            for (int i = 0; i < func.length(); i++) {
-                if (func.charAt(i) == 'R') {
-                    isReversed = !isReversed;
-                } else {
-                    if (deque.isEmpty()) {
-                        sb.append("error\n");
-                        isError = true;
-                        deque.clear();
-                        break;
-                    } else {
-                        if (isReversed) {
-                            deque.pollLast();
-                        } else {
-                            deque.pollFirst();
-                        }
-                        size--;
-                    }
-                }
-            }
-
-            if (!isError) {
-                sb.append("[");
-                StringJoiner sj = new StringJoiner(",");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        
+        int tc = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= tc; t++) {
+            n = Integer.parseInt(br.readLine());
+            arr = new int[n][n];
+            check = new boolean[n][n];
+            for (int i = 0; i < n; i++) {
+                String[] input = br.readLine().split("");//입력받기 붙어있는 숫자열
+                for (int j = 0; j < n; j++)
+                    arr[i][j] = Integer.parseInt(input[j]);
+            }sum = 0;
+            dfs(n/2,n/2);
+            System.out.println("#"+t+" "+sum);
+        }
+    }
+    public static void dfs(int x, int y) {
+        sum += arr[x][y];
+        check[x][y] =true;
+        for(int k = 0; k<4 ; k++){
+            int idx = x+ dx[k];
+            int idy = y+ dy[k];
+            if(0 <= idx && idx < n && 0<=idy && idy < n && !check[idx][idy]) {
+                dfs(idx,idy);
                 
-                for (int i = 0; i < size; i++) {
-                    sj.add(isReversed ? deque.pollLast() : deque.pollFirst());
-                }
-
-                sb.append(sj.toString()).append("]\n");
             }
         }
-
-        System.out.println(sb.toString());
     }
+
 }
--- baekjoon/곱셈_1629.java
+++ baekjoon/곱셈_1629.java
@@ -1,27 +1,40 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
+ 
 public class 곱셈_1629 {
+	public static long C;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    static int a, b, c, a2;
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        a = Integer.parseInt(st.nextToken()); b = Integer.parseInt(st.nextToken()); c =Integer.parseInt(st.nextToken());
-        a2 = 1;
-
-        if(b % 2 ==0) {
-            a *= a;
-            b /= 2;
-        } else {
-            a2 = a;
-            a *= a;
-            b /=2;
-        }
-        
-    }
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		C = Long.parseLong(st.nextToken());
+		
+		System.out.println(pow(A, B));
+	}
+	public static long pow(long A, long B) {
+		
+		// 지수가 1일 경우 A^1 이므로 A를 그대로 리턴
+		if(B == 1) {
+			return A % C;
+		}
+		
+		// 지수의 절반에 해당하는 A^(exponent / 2) 을 구한다.
+		long temp = pow(A, B / 2);
+		
+		/*
+		 * 현재 지수가 홀 수 였다면
+		 * A^(exponent / 2) * A^(exponent / 2) * A 이므로
+		 * A를 한 번 더 곱해주어야 한다.
+		 * 
+		 * ex) A^9 = A^4 * A^4 * A
+		 */
+		if(B % 2 == 1) {
+			return (temp * temp % C) * A % C;
+		}
+		return temp * temp % C;
+	}
 }
