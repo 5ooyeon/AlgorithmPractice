703ce83985dace88e9523756924c73777312824d
5ooyeon <melthetensai@gmail.com>
2024-03-29 18:02:23 +0900

    24.03.29

---
+++ baekjoon/트리순회_1991.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 트리순회_1991 {
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+    }
+}
